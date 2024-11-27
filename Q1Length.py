bbb32843e416b8649aee50528a4e586598001a59
Zachary Fowler <zachary.fowler1@stu.ocsb.ca>
2024-11-27 10:02:40 -0500

    Update Q1Length.py

--- Q1Length.py
+++ Q1Length.py
@@ -1,2 +1,12 @@
 # Create a program that will accept a word from a user and return the length of that word. 
 # Make this program in a loop with option to exit when the use types in quit.
+
+x = str(input ("Input a Word:  "))
+try:
+    z = int(x)
+    print ("Enter a Word")
+except ValueError:
+    y = len(x)
+    print ("our input was", y, "characters long")
+
+        
